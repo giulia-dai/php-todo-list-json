f399527b3965e29bc588f2b28545a3278dac3f6e
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-04-21 12:52:47 +0200

    creazione file.json + salvataggio task

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>to-do list</title>
 </head>
 
@@ -14,7 +15,10 @@
         <div class="container mt-5">
 
             <ul class="list-group">
-                <li v-for="todo in todoList" class="list-group-item" :class="{'text-decoration-line-through' :todo.done}">{{todo.text}}</li>
+                <li v-for="todo in todoList" class="list-group-item d-flex justify-content-between" :class="{'text-decoration-line-through' :todo.done}">
+                    {{todo.text}}
+                    <i class="fa-solid fa-trash-can"></i>
+                </li>
             </ul>
             <input @keyup.enter="addTodo()" type="text" v-model="todoItem.text" class="m-3" required>
             <button class="btn btn-primary m-3" @click="addTodo()">Aggiungi ToDo</button>
--- server.php
+++ server.php
@@ -1,40 +1,20 @@
 <?php
-$todoList = [
-    [
-        "text" => "HTML",
-        "done" => true
-    ],
-    [
-        "text" => "CSS",
-        "done" => false
-    ],
-    [
-        "text" => "Responsive design",
-        "done" => false
-    ],
-    [
-        "text" => "Javascript",
-        "done" => true
-    ],
-    [
-        "text" => "PHP",
-        "done" => true
-    ],
-    [
-        "text" => "Laravel",
-        "done" => false
-    ],
-    [
-        "text" => "Bootstrap",
-        "done" => true
-    ]
 
-];
+if (file_exists('database.json')) {
+    $string = file_get_contents('database.json');
+    $todoList = json_decode($string, true);
+} else {
+    $todoList = [];
+};
+
 if (isset($_POST['todoItem'])) {
     $todoList[] = [
         'text' =>  $_POST['todoItem']['text'],
         'done' => $_POST['todoItem']['done'] === "true" ? true : false
     ];
+
+    $myString = json_encode($todoList);
+    file_put_contents('database.json', $myString);
 }
 
 header('Content-Type: application/json');
